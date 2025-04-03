c24c7274408a628fad4e196951ae894b437852c7
Phaedra Sanon <71579580+ph4iry@users.noreply.github.com>
2025-04-03 11:14:20 -0400

    added landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,35 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+
+import localFont from 'next/font/local'
 import "./globals.css";
+import Header from "@/components/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const gotham = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Gotham/Gotham-Book.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Gotham/Gotham-BookItalic.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Gotham/Gotham-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Gotham/Gotham-BoldItalic.otf',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+  display: "swap",
+  variable: "--font-gotham"
 });
 
 export const metadata: Metadata = {
@@ -25,8 +45,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${gotham.variable} antialiased bg-[#]`}
       >
+        <Header />
         {children}
       </body>
     </html>
